7180e94d5e0babb5ca075525ab945f187857e93a
Luther Isaboke <lusaboke@gmail.com>
2024-12-24 15:08:00 +0300

    completed functions assignment

---
+++ lib/functions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    greeting=("Hello, programmer!")
+    print( greeting)
+
+def greet(name):
+    name="Guido"
+    print(f"Hello, {name}!")
+    
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+    
+
+def halve(number):
+    half=number/2
+    return(half)
+def add(num1, num2):
+    num1=45
+    num2=55
+    return (sum([num1,num2]))
+
+
